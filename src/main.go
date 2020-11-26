9327e1624f7c6b508fb5f415b25870ba60ae2324
kim dongjin <1222kdj@gmail.com>
2020-11-26 22:46:06 +0900

    commit

--- src/main.go
+++ src/main.go
@@ -6,7 +6,38 @@ import (
 
 func main() {
 	fmt.Println("Hello world")
-	example.forExample1()
-	example.forExample2()
-	example.forExample3()
+	forExample1()
+	forExample2()
+	forExample3()
+}
+
+func forExample1() {
+	// for example1
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func forExample2() {
+	// for example2
+	sum, i := 0, 0
+	for i < 10 {
+		sum += i
+		i++
+	}
+	fmt.Println(sum)
+}
+
+func forExample3() {
+	sum, i := 0, 0
+	for {
+		if i >= 10 {
+			break
+		}
+		sum += i
+		i++
+	}
+	fmt.Println(sum)
 }
